354c91433ad74f79c5848b548f62ba70e6b82d5b
trilencetech <trilencetechnology@gmail.com>
2025-09-02 09:34:12 +0530

    Update Workspace Doctype

--- erpnext/accounts/doctype/account/account.py
+++ erpnext/accounts/doctype/account/account.py
@@ -34,44 +34,13 @@ class Account(NestedSet):
 		account_currency: DF.Link | None
 		account_name: DF.Data
 		account_number: DF.Data | None
-		account_type: DF.Literal[
-			"",
-			"Accumulated Depreciation",
-			"Asset Received But Not Billed",
-			"Bank",
-			"Cash",
-			"Chargeable",
-			"Capital Work in Progress",
-			"Cost of Goods Sold",
-			"Current Asset",
-			"Current Liability",
-			"Depreciation",
-			"Direct Expense",
-			"Direct Income",
-			"Equity",
-			"Expense Account",
-			"Expenses Included In Asset Valuation",
-			"Expenses Included In Valuation",
-			"Fixed Asset",
-			"Income Account",
-			"Indirect Expense",
-			"Indirect Income",
-			"Liability",
-			"Payable",
-			"Receivable",
-			"Round Off",
-			"Round Off for Opening",
-			"Stock",
-			"Stock Adjustment",
-			"Stock Received But Not Billed",
-			"Service Received But Not Billed",
-			"Tax",
-			"Temporary",
-		]
+		account_type: DF.Literal["", "Accumulated Depreciation", "Asset Received But Not Billed", "Bank", "Cash", "Chargeable", "Capital Work in Progress", "Cost of Goods Sold", "Current Asset", "Current Liability", "Depreciation", "Direct Expense", "Direct Income", "Equity", "Expense Account", "Expenses Included In Asset Valuation", "Expenses Included In Valuation", "Fixed Asset", "Income Account", "Indirect Expense", "Indirect Income", "Liability", "Payable", "Receivable", "Round Off", "Round Off for Opening", "Stock", "Stock Adjustment", "Stock Received But Not Billed", "Service Received But Not Billed", "Tax", "Temporary"]
 		balance_must_be: DF.Literal["", "Debit", "Credit"]
+		branch: DF.Data | None
 		company: DF.Link
 		disabled: DF.Check
 		freeze_account: DF.Literal["No", "Yes"]
+		ifsc_code: DF.Data | None
 		include_in_gross: DF.Check
 		is_group: DF.Check
 		lft: DF.Int
--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -26,7 +26,7 @@ fixtures = [
     {"doctype": "Server Script"},
     {"doctype": "Role"},
     {"doctype": "Role Profile"},
-    {"doctype": "Module§ Profile"},
+    {"doctype": "Module Profile"},
     {"doctype": "Custom DocPerm"},
     {"doctype": "Email Template"},
     {"doctype": "Print Format"},
