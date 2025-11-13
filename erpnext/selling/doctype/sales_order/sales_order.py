7fbb605b443eca82289df7edfab5fd584588e7bf
trilencetech <trilencetechnology@gmail.com>
2025-11-13 09:36:10 +0530

    Update Report label

--- erpnext/selling/doctype/sales_order/sales_order.py
+++ erpnext/selling/doctype/sales_order/sales_order.py
@@ -372,24 +372,6 @@ class SalesOrder(SellingController):
     def validate_warehouse(self):
         super().validate_warehouse()
 
-        for d in self.get("items"):
-            if (
-                    (
-                        frappe.get_cached_value(
-                            "Item", d.item_code, "is_stock_item") == 1
-                        or (
-                            self.has_product_bundle(d.item_code)
-                            and self.product_bundle_has_stock_item(d.item_code)
-                        )
-                    )
-                    and not d.warehouse
-                    and not cint(d.delivered_by_supplier)
-            ):
-                frappe.throw(
-                    _("Delivery warehouse required for stock item {0}").format(
-                        d.item_code), WarehouseRequired
-                )
-
     def validate_with_previous_doc(self):
         super().validate_with_previous_doc(
             {
