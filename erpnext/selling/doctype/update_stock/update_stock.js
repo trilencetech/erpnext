15d3665288d97bc95e0eef0d5e8ff22a7f3fc34b
trilencetech <trilencetechnology@gmail.com>
2026-05-28 10:18:27 +0530

    Update Print Format

--- erpnext/selling/doctype/update_stock/update_stock.js
+++ erpnext/selling/doctype/update_stock/update_stock.js
@@ -11,8 +11,22 @@ frappe.ui.form.on("Update Stock", {
                 _open_print(frm, "Update Stock - Cash Bill");
             }, __("🖨️ Print"));
         }
+        _recalc(frm);
     },
+
+    total_amount(frm) { _recalc(frm); },
+    freight_amount(frm) { _recalc(frm); },
+    tax_rate(frm) { _recalc(frm); },
 });
+
+function _recalc(frm) {
+    var freight = flt(frm.doc.freight_amount || 0);
+    var tax_rate = flt(frm.doc.tax_rate != null ? frm.doc.tax_rate : 9);
+    var net = flt(frm.doc.total_amount || 0) + freight;
+    var tax = flt(net * tax_rate / 100, 2);
+    frm.set_value("tax_amount", tax);
+    frm.set_value("grand_total", flt(net + tax, 2));
+}
 
 function _open_print(frm, format_name) {
     var url = frappe.urllib.get_full_url(
--- erpnext/selling/doctype/update_stock/update_stock.py
+++ erpnext/selling/doctype/update_stock/update_stock.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class UpdateStock(Document):
@@ -18,10 +19,24 @@ class UpdateStock(Document):
         amended_from: DF.Link | None
         company: DF.Link
         customer: DF.Link
+        freight_amount: DF.Currency
+        grand_total: DF.Currency
         items: DF.Table[UpdateStockItem]
+        tax_amount: DF.Currency
+        tax_rate: DF.Float
         total_amount: DF.Currency
         total_qty: DF.Float
     # end: auto-generated types
+
+    def validate(self):
+        self._calculate_tax_totals()
+
+    def _calculate_tax_totals(self):
+        freight = flt(self.freight_amount or 0)
+        tax_rate = flt(self.tax_rate if self.tax_rate is not None else 9)
+        net_before_tax = flt(self.total_amount or 0) + freight
+        self.tax_amount = flt(net_before_tax * tax_rate / 100, 2)
+        self.grand_total = flt(net_before_tax + self.tax_amount, 2)
 
     def on_submit(self):
         for row in self.items:
